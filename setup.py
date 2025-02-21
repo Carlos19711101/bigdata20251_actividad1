94e297f90967c239b68269d313d86b07958b36f2
carlos <carlos.velez@est.iudigital.edu.co>
2025-02-21 14:38:58 -0500

    primera carga

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup;
+
+setup(
+    name="bigdata20251_actividad1",
+    version="0.0.1",
+    author="Carlos Velez",
+    author_email="",
+    description="",
+    py_modules=["antividad_1"],
+    install_requires=[
+    "pandas",
+    "openpyxl"  
+    ]
+
+)
